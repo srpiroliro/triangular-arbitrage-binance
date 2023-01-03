a199fe50eb83b7308bffe7b7d28996ace6b5a7f2
srpiroliro <srpiroliro21@gmail.com>
2023-01-03 15:02:10 +0100

    optimized + better working version

--- connections.py
+++ connections.py
@@ -1,6 +1,8 @@
 from pprint import pprint
 import requests,json
 
+from tqdm import tqdm
+
 # url="htthird_pairs://api.binance.com/api/v3/exchangeInfo"
 
 # symbols_req=requests.get(url=url)
@@ -15,40 +17,28 @@ import requests,json
 with open("symbols.json") as f:
     symbols=json.loads(f.read())
 
-def is_equal(p1,p2)->bool:
-    return (p1[0]==p2[0] and p1[1]==p2[1]) or (p1[0]==p2[1] and p1[1]==p2[0])
 
-# CHECK: is this necesorted_symbolsary?
-sorted_symbols={}
-for pair in symbols:
-    for x in pair:
-        if x in sorted_symbols: 
-            sorted_symbols[x].append(pair)
-        else: sorted_symbols[x]=[pair]
-
-starting=""
+starting="usd"
+ending=starting
 
 paths=[]
-for pair in symbols:
-    base,quote=pair # middle part
-
-    for first_pair in sorted_symbols[base]:
-        if not is_equal(pair,first_pair) and (not starting or starting in first_pair):
-            base1,quote1=first_pair
-            for third_pair in sorted_symbols[quote1]:
-                if quote in third_pair and (not starting or starting in third_pair) and not is_equal(third_pair,first_pair) and not is_equal(third_pair,pair):
-                    paths.append(["".join(first_pair),"".join(pair),"".join(third_pair)])
+for pair2 in tqdm(symbols):
+    txt_pair2="".join(pair2)
+    group=[None, txt_pair2, None]
     
-    for first_pair in sorted_symbols[quote]:
-        if not is_equal(pair,first_pair) and (not starting or starting in first_pair):
-            base1,quote1=first_pair
-            for third_pair in sorted_symbols[quote1]:
-                if base in third_pair and (not starting or starting in third_pair) and not is_equal(third_pair,first_pair) and not is_equal(third_pair,pair):
-                    paths.append(["".join(first_pair),"".join(pair),"".join(third_pair)])
+    for pair1 in symbols:
+        txt_pair1="".join(pair1)
+        if (pair1[0] in pair2)^(pair1[1] in pair2) and (starting in txt_pair1.lower() or not starting):
+            pair3=list(set(pair1)^set(pair2))
+            
+            if not pair3 in symbols: pair3.reverse()
+            if pair3 in symbols:
+                group[0]=txt_pair1
+                group[2]="".join(pair3)
+                
+                paths.append(group.copy())
 
 print(len(paths))
-# for path in paths:
-#     print(" - ".join(path))
     
 with open("paths.json","w") as f:
     json.dump(paths,f)
